141e59a97daafb51be8145e233dc562954cab2c9
Sergey Novikov <snov@snov.me>
2016-06-16 10:57:56 +0200

    Handle SIGTERM

--- start.sh
+++ start.sh
@@ -1,32 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
-name=${ES_NAME:-docker}
-host=${ES_HOST:-elasticsearch}
-port=${ES_PORT:-9200}
-username=${ES_USER}
-password=${ES_PASSWD}
-reconnects=${ES_RECONNECTS:-10}
+set -e
 
-./npi config set license_key $NEW_RELIC_LICENSE_KEY
-./npi prepare me.snov.newrelic-elasticsearch -n -q
+if [ "$1" = 'newrelic-elasticsearch' ]; then
+	name=${ES_NAME:-docker}
+	host=${ES_HOST:-elasticsearch}
+	port=${ES_PORT:-9200}
+	username=${ES_USER}
+	password=${ES_PASSWD}
+	reconnects=${ES_RECONNECTS:-10}
 
-sed -i "s/%HOST%/$host/g" plugin.json
-sed -i "s/%PORT%/$port/g" plugin.json
-sed -i "s/%NAME%/$name/g" plugin.json
-sed -i "s/%USERNAME%/$username/g" plugin.json
-sed -i "s/%PASSWORD%/$password/g" plugin.json
-cp plugin.json `find . -path './plugins/me.snov.newrelic-elasticsearch/*/config/plugin.json'`
+	./npi config set license_key $NEW_RELIC_LICENSE_KEY
+	./npi prepare me.snov.newrelic-elasticsearch -n -q
 
-for i in `seq 1 $reconnects`
-do
- 	echo -n "Trying $host:$port... "
-	if curl --silent "$host:$port" > /dev/null
-	then
- 		echo "OK"
-		break
- 	fi
-	echo "NOT AVAILABLE"
-	sleep 5
-done
+	sed -i "s/%HOST%/$host/g" plugin.json
+	sed -i "s/%PORT%/$port/g" plugin.json
+	sed -i "s/%NAME%/$name/g" plugin.json
+	sed -i "s/%USERNAME%/$username/g" plugin.json
+	sed -i "s/%PASSWORD%/$password/g" plugin.json
+	cp plugin.json `find . -path './plugins/me.snov.newrelic-elasticsearch/*/config/plugin.json'`
 
-./npi start me.snov.newrelic-elasticsearch --foreground
+	for i in `seq 1 $reconnects`
+	do
+	 	echo -n "Trying $host:$port... "
+		if curl --silent "$host:$port" > /dev/null
+		then
+	 		echo "OK"
+			break
+	 	fi
+		echo "NOT AVAILABLE"
+		sleep 5
+	done
+
+	exec ./npi start me.snov.newrelic-elasticsearch --foreground
+fi
+
+exec "$@"
